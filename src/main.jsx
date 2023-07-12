44e013547c20eaff2f7a88c40610238fd6e34ff8
Diego Pais <drborges.pais@gmail.com>
2023-07-12 10:12:38 -0300

    adding vans route with miragejs

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import './index.css';
 
 import { Home } from './pages/Home';
 import { About } from './pages/About';
+import { Vans } from './pages/Vans';
+
+import '../server';
 
 function App() {
   return (
@@ -16,11 +19,13 @@ function App() {
         </Link>
         <nav>
           <Link to='/about'>About</Link>
+          <Link to='/vans'>Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
