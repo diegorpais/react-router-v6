278ca67f4131b7b93ee7d0277eeafa6cc50755e1
Diego Pais <drborges.pais@gmail.com>
2023-07-12 13:45:24 -0300

    van detail

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './index.css';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Vans } from './pages/Vans';
+import { VanDetail } from './pages/VanDetail';
 
 import '../server';
 
@@ -26,6 +27,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/vans' element={<Vans />} />
+        <Route path='/vans/:id' element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export function Vans() {
   const [vans, setVans] = React.useState([]);
-  
+
   React.useEffect(() => {
     fetch('/api/vans')
       .then((res) => res.json())
@@ -11,15 +12,17 @@ export function Vans() {
 
   const vanElements = vans.map((van) => (
     <div key={van.id} className='van-tile'>
-      <img src={van.imageUrl} />
-      <div className='van-info'>
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
-      </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className='van-info'>
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
     </div>
   ));
 
